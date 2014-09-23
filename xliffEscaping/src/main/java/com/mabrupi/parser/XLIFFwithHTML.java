0b394753755cad15bc3f2074fb6e1cafbcfb8209
ManelBrull <mbrull88@gmail.com>
2014-09-24 00:03:07 +0200

    Added more guards and assuming only one source block

--- xliffEscaping/src/main/java/com/mabrupi/parser/XLIFFwithHTML.java
+++ xliffEscaping/src/main/java/com/mabrupi/parser/XLIFFwithHTML.java
@@ -13,9 +13,22 @@ public class XLIFFwithHTML {
 		if(parse == null) return "";
 		if(parse.isEmpty()) return "";
 		if(!parse.contains("<source>")) return parse;
+		return getParseResult(parse);
+	}
+	
+	private String getParseResult(String toParse){
 		StringBuffer result = new StringBuffer();
-					
-		return "";
+		//We assume an alone source block
+		int iniPosSource = toParse.indexOf("<source>"); 
+		int finPosSource = toParse.indexOf("</source>");
+		result.append(toParse.substring(0, iniPosSource));
+		result.append(escapeHTML(toParse.substring(iniPosSource, finPosSource)));
+		result.append(toParse.substring(finPosSource));
+		return result.toString();
+	}
+	
+	private String escapeHTML(String toEscape){
+		return toEscape;
 	}
 	
 }
