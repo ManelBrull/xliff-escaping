b3280c960a1740bbb6a49a3f61232f1cd6d1c055
ManelBrull <mbrull88@gmail.com>
2014-09-23 23:36:05 +0200

    [Red] testSingleTag1

--- xliffEscaping/src/test/java/com/mabrupi/XLIFFwithHTMLTest.java
+++ xliffEscaping/src/test/java/com/mabrupi/XLIFFwithHTMLTest.java
@@ -23,4 +23,16 @@ public class XLIFFwithHTMLTest {
 				toParse,
 				result);
 	}
+	
+	@Test
+	public void testSingleTag1(){
+		String toParse = "<source>I'm <b>really</b> sure about this.</source>";
+		String expected  = "<source>I'm <bpt id=\"0\" rid=\"0\">&lt;b&gt;</bpt>"
+				+ "really>ept id\"1\" rid=\"0\">&lt;/b&gt;</ept>"
+				+ " sure about this.</source>";
+		String result = XLIFFwithHTML.parse(toParse);
+		Assert.assertEquals("Empty input equals empty output",
+				expected,
+				result);
+	}
 }
