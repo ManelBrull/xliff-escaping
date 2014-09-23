67cad71f84d02a8def9eb9d5faed7fcf85476c39
ManelBrull <mbrull88@gmail.com>
2014-09-23 23:03:32 +0200

    [Green] testEmpty

--- xliffEscaping/src/test/java/com/mabrupi/XLIFFwithHTMLTest.java
+++ xliffEscaping/src/test/java/com/mabrupi/XLIFFwithHTMLTest.java
@@ -1,14 +1,26 @@
 package com.mabrupi;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.mabrupi.parser.XLIFFwithHTML;
 
 public class XLIFFwithHTMLTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmpty() {
+		String result = XLIFFwithHTML.parse("");
+		Assert.assertEquals("Empty input equals empty output",
+				"",
+				result);
 	}
-
+	
+	/**@Test
+	public void testHelloWorld() {
+		String toParse = "<source>Hello world</source>";
+		String result = XLIFFwithHTML.parse(toParse);
+		Assert.assertEquals("Empty input equals empty output",
+				toParse,
+				result);
+	}**/
 }
